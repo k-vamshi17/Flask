434eb369fe579adbc4fd1e2dd6ff4c11aa9092ff
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2024-12-08 22:11:02 +0530

    2nd

--- flask1.py
+++ flask1.py
@@ -10,4 +10,11 @@ def hello_world1():
     <h2>Hello This is first Flask Application</h2>
     </body>'''
     return output
+
+def hello_world2():
+    output='''<body bgcolor=aqua>
+    <h2>Hello This is First Flask Application</h2>
+    </body>'''
+    return output
+app.add_url_rule('/hello2','hello_world2',hello_world2)
 app.run()
